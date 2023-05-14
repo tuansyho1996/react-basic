6dec2e14638aeaf5c8b3bef9843f542b2cee5a97
tuansyho <emberfidestore@gmail.com>
2023-05-14 15:05:12 +0700

    video 4 component

---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>Hello reactjs basic</div>
+        )
+    }
+}
+export default MyComponent;
