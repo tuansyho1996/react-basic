c9f043b8579fcce3401e5f5a8ff7a4495b06a23d
tuansyho <emberfidestore@gmail.com>
2023-05-22 13:50:53 +0700

    video 33 mapping state(redux) to props(react)

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Color from "../HOC/Color";
-import logo from "../../assets/images/logo.jpg"
+import logo from "../../assets/images/logo.jpg";
+import { connect } from "react-redux";
 
 class Home extends React.Component {
     // componentDidMount() {
@@ -9,11 +10,24 @@ class Home extends React.Component {
     //     }, 5000)
     // }
     render() {
+        let { dataRedux } = this.props
+        console.log(dataRedux)
         return (
             <div>
                 <img style={{ height: '200px', width: '200px', marginTop: '20px' }} src={logo} alt="logo" />
+                {
+                    dataRedux.map((item) => {
+                        return (<h2 key={item.id}>Name:{item.name}</h2>)
+                    })
+                }
             </div>
         )
     }
 }
-export default Color(Home);
+const mapStateProps = (state) => {
+    return {
+        dataRedux: state.users
+    }
+}
+
+export default connect(mapStateProps)(Color(Home));
